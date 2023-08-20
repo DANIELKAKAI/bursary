994ce3dac8860427937338721e1dbb2f808cd6c0
dankakai <daniel.kakai@twiga.com>
2023-08-20 16:07:58 +0300

    auth and detail upload

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -29,7 +29,7 @@ function register() {
                     email,
                     role
                 });
-                console.log("Register Success");
+                location.href = "login.html"
             }).catch((err) => {
                 alert(err.message);
             });
@@ -39,18 +39,103 @@ function register() {
 }
 
 
-const passwordInput = document.getElementById('password');
-const password2Input = document.getElementById('password2');
-const passwordError = document.getElementById('password-error');
+function logIn() {
 
-password2Input.addEventListener('input', () => {
-    const password = passwordInput.value;
-    const password2 = password2Input.value;
+    const loginForm = document.getElementById('login-form');
+    loginForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
 
-    if (password === password2) {
-        passwordError.textContent = '';
-    } else {
-        passwordError.textContent = "Passwords don't match";
+        const email = loginForm.email.value;
+        const password = loginForm.password.value;
+        const passwordError = document.getElementById('password-error');
+
+        if (email && password) {
+            firebase.auth().signInWithEmailAndPassword(email, password)
+                .then((userCredential) => {
+                    passwordError.textContent = "";
+                    const queryParams = new URLSearchParams(window.location.search);
+                    location.href = queryParams.get("next") ? queryParams.get("next") : "home.html";
+                })
+                .catch((error) => {
+                    passwordError.textContent = `${error.message}`;
+                });
+        };
+    });
+};
+
+function logOut() {
+    firebase.auth().signOut().then(() => {
+        location.href = "home.html";
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
+function checkAuth(role) {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (!user) {
+            location.href = `login.html?next=${window.location.pathname}`;
+        }
+        autoFillUser(user);
+        firebase.firestore().collection("users").doc(user.uid).get().then((doc) => {
+            if (doc.exists) {
+                const userData = doc.data();
+                if (userData.role != role) {
+                    location.href = "404.html";
+                }
+            }
+        });
+    });
+};
+
+function submitDetails() {
+
+    const applyForm = document.getElementById('apply-form');
+    applyForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        const inputElements = applyForm.querySelectorAll("input");
+
+        const uid = document.getElementById('uid').value;
+
+        const inputValues = { date: currentDate() };
+
+        inputElements.forEach((input) => {
+            inputValues[input.name] = input.value;
+        });
+
+        //console.log({ date: currentDate(), ...inputNames });
+
+        firebase.firestore().collection("student-details").doc(uid).set(
+            inputValues
+        )
+    })
+
+}
+
+function autoFillUser(user) {
+    const form = document.getElementById("apply-form");
+
+    if (form) {
+        const emailInput = document.getElementById("email");
+
+        if (emailInput) {
+            emailInput.value = user.email;
+        }
+
+        const uidInput = document.getElementById("uid");
+        if (uidInput) {
+            uidInput.value = user.uid;
+        }
     }
-});
 
+}
+
+function currentDate() {
+    const currentDate = new Date();
+
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const year = currentDate.getFullYear();
+    return `${day}-${month}-${year}`;
+}
